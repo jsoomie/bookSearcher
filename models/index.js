4bfab57954c360211b1b0ef4329377bc515cec5a
Jonathan Soukaseume <sksmejn@gmail.com>
2021-05-08 16:47:28 -0700

    added models exports index

--- models/index.js
+++ models/index.js
@@ -1,28 +1,3 @@
-const { Schema, model } = require("mongoose");
-
-const bookSchema = new Schema({
-  title: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  authors: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  image: {
-    type: String,
-    required: true,
-  },
-  link: {
-    type: String,
-    required: true,
-  },
-});
-
-module.exports = model("Book", bookSchema);
+module.exports = {
+  Book: require("./Book"),
+};
