8a122c780719cb1529c2d57708b71ebb12f6f50c
Jonathan Soukaseume <sksmejn@gmail.com>
2021-05-09 09:36:24 -0700

    got the google api to return soemthing useable and got the book controller working with database

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
 const express = require("express");
 const app = express();
 const compression = require("compression");
-const db = require("./config/connection");
+const connection = require("./config/connection");
 
 const PORT = process.env.PORT || 3001;
 
 // Database connect
-db();
+connection();
 
 // Middleware
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Routes
+const { findAll } = require("./controllers");
+
+app.use("/", findAll);
 
 app.listen(PORT, (err) => {
   if (err) console.log(err.message);
