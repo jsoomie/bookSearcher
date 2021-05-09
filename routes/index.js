c03a01c7a18db55ce042d75c107938026d604b86
Jonathan Soukaseume <sksmejn@gmail.com>
2021-05-09 10:01:20 -0700

    added google routes to fetch google api books and book database routes

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,8 @@
 const router = require("express").Router();
 const bookRoutes = require("./bookRoutes");
+const googleRoutes = require("./googleRoutes");
 
 router.use("/book", bookRoutes);
+router.use("/google", googleRoutes);
+
+module.exports = router;
